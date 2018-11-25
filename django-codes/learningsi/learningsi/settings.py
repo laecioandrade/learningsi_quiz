c6c953c31efb99a394a246af85efd34f2dd4ecf2
Thalesousa <thalestjsb@gmail.com>
2018-11-25 19:20:09 -0300

    banco em mysql

--- django-codes/learningsi/learningsi/settings.py
+++ django-codes/learningsi/learningsi/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'learningsi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_tp',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -93,14 +97,10 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
